ae79e6be6894bd6c53332fa54311b8c497f69ab9
kabircse <=>
2016-10-20 17:23:14 +0600

    added single post

---
+++ assets/js/custom.js
@@ -0,0 +1,4 @@
+$(function(){
+  //sidebar tab show/hide
+  $('#sidebar-tab a:first').tab('show');
+});
---
+++ assets/slider/slider.js
@@ -0,0 +1,6 @@
+$(function(){
+  //For Carsouel slider on single post
+  // manual carousel controls
+  $('.next').click(function(){ $('.carousel').carousel('next');return false; });
+  $('.prev').click(function(){ $('.carousel').carousel('prev');return false; });
+});
