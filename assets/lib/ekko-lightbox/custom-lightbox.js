1ad0c6c08615ac4756fbe0d21f5171d127518985
Kabir Hossain <kabir.cse10@gmail.com>
2016-10-23 23:16:48 +0600

    portfolio completed with sort and lightbox

---
+++ assets/lib/ekko-lightbox/custom-lightbox.js
@@ -0,0 +1,19 @@
+
+    $(document).ready(function ($) {
+        // delegate calls to data-toggle="lightbox"
+        $(document).on('click', '[data-toggle="lightbox"]:not([data-gallery="navigateTo"])', function(event) {
+            event.preventDefault();
+            return $(this).ekkoLightbox({
+                onShown: function() {
+                    if (window.console) {
+                        return console.log('Checking our the events huh?');
+                    }
+                },
+                onNavigate: function(direction, itemIndex) {
+                    if (window.console) {
+                        return console.log('Navigating '+direction+'. Current item: '+itemIndex);
+                    }
+                }
+            });
+        });
+    });
--- assets/lib/slider/slider.js
+++ assets/lib/slider/slider.js
@@ -1,5 +1,5 @@
 $(function(){
-  //For Carsouel slider on single post
+  //small slider like single-post.php
   // manual carousel controls
   $('.next').click(function(){ $('.carousel').carousel('next');return false; });
   $('.prev').click(function(){ $('.carousel').carousel('prev');return false; });
