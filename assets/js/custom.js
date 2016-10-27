30b5336c7de25a843fec22cfd4bda5b29329e798
kabircse <=>
2016-10-27 17:08:59 +0600

    modify single-item

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,6 @@
 $(function() {
     //sidebar tab show/hide
-    $('#sidebar-tab a:first').tab('show');
+    $('#collapse-tab a:first').tab('show');
 
     //For using tooltip
     $('[data-toggle="tooltip"]').tooltip();
